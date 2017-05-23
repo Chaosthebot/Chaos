705af291a4e7ddbb366671757ca647bcd56b8e24
scaule <scaule@clever-age.com>
2017-05-23 23:12:08 +0200

    Use setting file on /etc

--- twitter_api/twitterApi.py
+++ twitter_api/twitterApi.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 
 import twitter
+import json
 
 class Twitter:
 
     def __init__(self):
-        consumer_key = "WXfZoJi7i8TFmrGOK5Y7dVHon"
-        consumer_secret = "EE46ezCkgKwy8GaKOFFCuMMoZbwDprnEXjhVMn7vI7cYaTbdcA"
-        access_key = "867082422885785600-AJ0LdE8vc8uMs21VDv2jrkwkQg9PClG"
-        access_secret = "qor8vV5kGqQ7mJDeW83uKUk2E8MUGqp5biTTswoN4YEt6"
+        with open('/etc/twitter.json') as data_file:
+            data = json.load(data_file)
         encoding = None
-        self.api = twitter.Api(consumer_key=consumer_key, consumer_secret=consumer_secret,
-                      access_token_key=access_key, access_token_secret=access_secret,
+        self.api = twitter.Api(consumer_key=data["consumer_key"], consumer_secret=data["consumer_secret"],
+                      access_token_key=data["access_key"], access_token_secret=data["access_secret"],
                       input_encoding=encoding)
 
     def postTweet(self,message):

59cd5881e6eaa33b0991107540bc0d576732ae65
Anthony Nixon <ajn0592@gmail.com>
2017-05-23 00:06:27 -0500

    remove a space i left in

--- github_api/comments.py
+++ github_api/comments.py
@@ -15,7 +15,7 @@ def get_reactions_for_comment(api, urn, comment_id, since):
 def leave_reject_comment(api, urn, pr):
     body = """
 :no_good: This PR did not meet the required vote threshold and will not be merged. \
-Closing. 
+Closing.
 
 Open a new PR to restart voting.
     """.strip()

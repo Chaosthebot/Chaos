1911a60c24b4d1d1bbc71b5df327495db789a7dd
Jonathan Pepler <jonpepler@Jons-MacBook-Pro.local>
2017-05-24 23:11:35 +0100

    Change purpose from revert PRs to merging random PRs as a fallback

--- cron/check_fallback.py
+++ cron/check_fallback.py
@@ -5,6 +5,8 @@ import settings
 import github_api as gh
 
 __log = logging.getLogger("chaosbot")
+api = gh.API(settings.GITHUB_USER, settings.GITHUB_SECRET)
+urn = settings.URN
 
 def  check_fallback():
     latest_commit = Commit(gh.get_latest_commit(api, settings.URN))
@@ -12,8 +14,8 @@ def  check_fallback():
     now = arrow.utcnow()
     span = now - commit_time
     if(span.seconds < setings.FALLBACK_WINDOW_SECONDS)
-        revert_last_commit(latest_commit.get_parent_sha())
-
-def revert_last_commit(sha):
+        # it happened
+        message = "No pull requests were approved in the last {fallback_hours} hours. Therefore, a random compatible PR was chosen to be merged in spite of current voting status.".format(fallback_hours=settings.FALLBACK_WINDOW_HOURS)
+        gh.merge_random(api, urn, message)
 
 

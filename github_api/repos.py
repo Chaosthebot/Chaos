0b2348514785dba094202ae31671b733bb45dc74
Jon Pepler <jon1467@users.noreply.github.com>
2017-05-24 23:34:50 +0100

    Remove unused line

--- github_api/repos.py
+++ github_api/repos.py
@@ -9,10 +9,6 @@ def get_latest_commit(api, urn):
     path = "/repos/{urn}/commits".format(urn=urn)
     data = api("get", path)
     if(len(data) > 0)
-      # return previous commit sha if it exists
-      if(len(data) >= 1)
-        data[0]['last_commit_sha'] =
-      else
         return data[0]
     else
         return None

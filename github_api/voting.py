fd492d3bfb6dc552fd89902db5a488e5ecdce8bc
ECrownofFire <ecrownoffire@gmail.com>
2017-05-23 21:17:58 -0400

    Fix typo

--- github_api/voting.py
+++ github_api/voting.py
@@ -194,8 +194,8 @@ def get_voting_window(now):
     lhour = local.hour
 
     hours = settings.DEFAULT_VOTE_WINDOW
-    if (settings.AFTER_HOURS_START >= lhours or
-            settings.AFTER_HOURS_END <= lhours):
+    if (settings.AFTER_HOURS_START >= lhour or
+            settings.AFTER_HOURS_END <= lhour):
         hours = settings.AFTER_HOURS_VOTE_WINDOW
 
     seconds = hours * 60 * 60

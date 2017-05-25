ccbcbc24021cb9385a7f1641ccead23fc9e70e77
Adam Dobrawy <naczelnik@jawnosc.tk>
2017-05-25 18:36:36 +0200

    Add protection to syntax errors

--- startup.sh
+++ startup.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
+
+python -m py_compile $(find . -name '*.py');
+if [ "$?" != "0" ]
+then
+  echo "There is syntax error. This may endanger chaos. Pauses updates."
+  exit
+fi
+
 for file in startup.d/*; do
   [[ -f "$file" && -x "$file" ]] && "$file"
 done

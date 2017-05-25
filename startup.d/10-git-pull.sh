093c71f623fb43a1792a80a7ae0fe56e0e5b34e9
Adam Dobrawy <naczelnik@jawnosc.tk>
2017-05-25 19:41:36 +0200

    Prevent code updates if malicious changes

--- startup.d/10-git-pull.sh
+++ startup.d/10-git-pull.sh
@@ -1,2 +1,21 @@
 #!/bin/sh
+
+# Prepare code auditing environment
+REPO_URL=$(git remote get-url origin)
+TEST_DIR=$(mktemp -d)
+git clone "$REPO_URL" "$TEST_DIR";
+pushd "$TEST_DIR";
+
+# Code auditing section
+python -m py_compile $(find . -name '*.py');
+if [ "$?" != "0" ]
+then
+  echo "There is syntax error. This may endanger chaos. Pauses updates."
+  exit 45
+fi
+
+# End code auditing section
+popd
+
+git checkout master
 git pull
--- startup.d/20-syntax-check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python -m py_compile $(find . -name '*.py');
-if [ "$?" != "0" ]
-then
-  echo "There is syntax error. This may endanger chaos. Pauses updates."
-  killall bash
-fi

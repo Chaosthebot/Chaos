18546a5aca1f5cdd120fef75a2f2db6a70a47cfc
Alex Bradley <abrad12122@gmail.com>
2017-05-26 22:43:03 -0500

    Moved rpi Docker files to /dev/docker

--- dev/docker/rpi/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp ../../../requirements.txt .
-docker build -t chaos .
-rm requirements.txt
--- dev/docker/rpi/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker run -it --rm\
-    -v $(cd ../../; pwd):/root/workspace/Chaos\
-    -p 8082:80\
-    -p 8081:8081\
-    chaos

87e0f0026191301261dcaeef3d9fbea4f2e750cb
Phil Rukin <philipp@rukin.me>
2017-05-30 08:52:31 +0200

    merging PR #383: 🤓📈 Use sigmoid function to calculate dynamic_voting_window
    https://github.com/chaosbot/Chaos/pull/383: 🤓📈 Use sigmoid function to calculate dynamic_voting_window
    
    Description:
    - updated tests
    - reduced `EXTENDED_VOTE_WINDOW` to 8
    
    [Interactive graphic](https://www.wolframalpha.com/input/?i=plot+8%2F(e%5E(-x%2F12)+%2B+1)+,++x+%3D+-0..60)
    
    Not so interactive:
    ![image](https://cloud.githubusercontent.com/assets/577316/26565838/113bdf5c-44ef-11e7-9ca7-58a56c0c8262.png)
    
    :ok_woman: PR passed with a vote of 11 for and 0 against, with a weighted total             of 11.0 and a threshold of 6.7.
    
    Vote record:
    @N00D13: 1
    @amoffat: 1
    @andrewda: 1
    @davidak: 1
    @hongaar: 1
    @lifecircles: 1
    @mark-i-m: 1
    @mgardne8: 1
    @phil-r: 1
    @rudehn: 1
    @tarunbatra: 1

--- github_api/misc.py
+++ github_api/misc.py
@@ -1,4 +1,4 @@
-from math import log
+from math import exp
 
 
 def dt_to_github_dt(dt):
@@ -12,12 +12,21 @@ def seconds_to_human(seconds):
 
 
 def dynamic_voting_window(x, lbound, ubound):
-    """ determine the number of hours for the voting window
+    """ Determine the number of hours for the voting window
     based on the number of day since the project start
-    return between a lower and upper bound """
+    return between a lower and upper bound using sigmoid function.
+    Sigmoid functions never reach their upper bound.
+    With current settings it will "almost" reach ubound in ~60 days (x=60)"""
 
-    # current settings reaches ubound in ~50 days (x=50)
-    multiplier = 21
-    log_base = 2
+    # to make curve more flat
+    modified_x = x / 12
 
-    return min(log(max(x * multiplier, 1), log_base) + lbound, ubound)
+    # calculate parameters
+    difference = ubound - lbound
+    param_a = difference * 2
+    param_b = lbound - difference
+
+    # calculate sigmoid function
+    sigmoid = 1 / (exp(-modified_x) + 1)
+
+    return param_a * sigmoid + param_b
--- tests/github_api/misc_test.py
+++ tests/github_api/misc_test.py
@@ -1,5 +1,6 @@
 import unittest
 import settings
+from math import inf
 from github_api import compute_api_cooldown as compute
 from github_api import voting
 
@@ -8,12 +9,12 @@ class CooldownTests(unittest.TestCase):
     def test_no_requests(self):
         """ no requests means always sleep to the reset time + padding """
         pad = settings.API_COOLDOWN_RESET_PADDING
-        self.assertEqual(60+pad, compute(0, 60))
-        self.assertEqual(61+pad, compute(0, 61))
-        self.assertEqual(62+pad, compute(0, 62))
+        self.assertEqual(60 + pad, compute(0, 60))
+        self.assertEqual(61 + pad, compute(0, 61))
+        self.assertEqual(62 + pad, compute(0, 62))
 
         # test a negative remaining as well
-        self.assertEqual(60+pad, compute(-1, 60))
+        self.assertEqual(60 + pad, compute(-1, 60))
 
     def test_longer_and_longer(self):
         """ fewer requests = longer wait """
@@ -25,8 +26,31 @@ class CooldownTests(unittest.TestCase):
 
     def test_dynamic_voting_window(self):
         """ test lower and upper bounds of dynamic voting window """
-        self.assertEqual(voting.dynamic_voting_window(0, 0, 10), 0)
-        self.assertEqual(voting.dynamic_voting_window(50, 0, 10), 10)
 
-        self.assertEqual(voting.dynamic_voting_window(0, 10, 20), 10)
-        self.assertEqual(voting.dynamic_voting_window(50, 10, 20), 20)
+        first_day = 0
+        in_2weeks = 14
+        in_2month = 60
+        far_future = inf  # <- we will never reach this in reality
+        print(voting.dynamic_voting_window(15, 10, 20))
+
+        # first day starts at the lower bound
+        self.assertEqual(voting.dynamic_voting_window(first_day, 0, 10), 0)
+        self.assertEqual(voting.dynamic_voting_window(first_day, 10, 10), 10)
+        self.assertEqual(voting.dynamic_voting_window(first_day, 10, 20), 10)
+
+        # in 2 weeks should be somewhere in between
+        window = voting.dynamic_voting_window(in_2weeks, 0, 10)
+        self.assertTrue(window > 1)
+        self.assertTrue(window < 9)
+
+        # in 2 month should start reaching upper bound
+        # assertAlmostEqual with 0 as a 3rd argument rounds the values
+        self.assertAlmostEqual(voting.dynamic_voting_window(in_2month, 0, 10), 10, 0)
+        self.assertAlmostEqual(voting.dynamic_voting_window(in_2month, 10, 10), 10, 0)
+        self.assertAlmostEqual(voting.dynamic_voting_window(in_2month, 10, 20), 20, 0)
+
+        # many days in the future reaches upper bound
+        # assertEqual also works here, but why risk?
+        self.assertAlmostEqual(voting.dynamic_voting_window(far_future, 0, 10), 10)
+        self.assertAlmostEqual(voting.dynamic_voting_window(far_future, 10, 10), 10)
+        self.assertAlmostEqual(voting.dynamic_voting_window(far_future, 10, 20), 20)

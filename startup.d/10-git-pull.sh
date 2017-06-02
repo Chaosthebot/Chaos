d5a9e32aecd1f58b4c3e1a51d5fbb118bb609975
rudehn <nrude93@gmail.com>
2017-06-02 16:29:30 -0400

    merging PR #487: remove broken auditing
    https://github.com/chaosbot/Chaos/pull/487: remove broken auditing
    
    Description:
    as seen by 
    ```
    error: Unknown subcommand: get-url
    usage: git remote [-v | --verbose]
       or: git remote add [-t <branch>] [-m <master>] [-f] [--tags|--no-tags] [--mirror=<fetch|push>] <name> <url>
       or: git remote rename <old> <new>
       or: git remote remove <name>
       or: git remote set-head <name> (-a | --auto | -d | --delete |<branch>)
       or: git remote [-v | --verbose] show [-n] <name>
       or: git remote prune [-n | --dry-run] <name>
       or: git remote [-v | --verbose] update [-p | --prune] [(<group> | <remote>)...]
       or: git remote set-branches [--add] <name> <branch>...
       or: git remote set-url [--push] <name> <newurl> [<oldurl>]
       or: git remote set-url --add <name> <newurl>
       or: git remote set-url --delete <name> <url>
    
        -v, --verbose         be verbose; must be placed before a subcommand
    
    fatal: The empty string is not a valid path
    startup.d/10-git-pull.sh: 7: startup.d/10-git-pull.sh: pushd: not found
    startup.d/10-git-pull.sh: 19: startup.d/10-git-pull.sh: popd: not found
    Already on 'master'
    ```
    
    With travis implemented, this is not needed.
    
    :ok_woman: PR passed with a vote of 9 for and 0 against, a weighted total of 8.5 and a threshold of 6.6, and a current meritocracy review.
    
    Vote record:
    @Ayyko: 1
    @PlasmaPower: 1
    @Smittyvb: 1
    @amoffat: 1
    @andrewda: 1
    @md678685: 1
    @phil-r: 1
    @rhengles: 1
    @rudehn: 1

--- startup.d/10-git-pull.sh
+++ startup.d/10-git-pull.sh
@@ -1,23 +1,3 @@
 #!/bin/sh
 
-# Prepare code auditing environment
-REPO_URL=$(git remote get-url origin)
-TEST_DIR=$(mktemp -d)
-git clone "$REPO_URL" "$TEST_DIR";
-pushd "$TEST_DIR";
-
-# Code auditing section
-find . -name '*.py' -print0 | xargs -0 python -m py_compile;
-if [ "$?" != "0" ]
-then
-  echo "There is syntax error. This may endanger chaos. Pauses updates."
-  rm -r -f "$TEST_DIR";
-  exit 45
-fi
-
-# End code auditing section
-popd
-rm -r -f "$TEST_DIR";
-
-git checkout master
 git pull
